26cdbc19e9d242fe91f2e04bceaf7866de0fa390
Rasna <rasnack7@gmail.com>
2024-03-03 22:55:35 +0530

    completed the post request

--- lib/screens/add_page.dart
+++ lib/screens/add_page.dart
@@ -1,4 +1,7 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
 
 class AddTodoPage extends StatefulWidget {
   const AddTodoPage({super.key});
@@ -8,6 +11,9 @@ class AddTodoPage extends StatefulWidget {
 }
 
 class _AddTodoPageState extends State<AddTodoPage> {
+  TextEditingController titileController = TextEditingController();
+  TextEditingController descriptionController = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -16,16 +22,70 @@ class _AddTodoPageState extends State<AddTodoPage> {
       ),
       body: ListView(
         children: [
-          TextField(
-            decoration: InputDecoration(hintText: "Title"),
-          ),
-          TextField(
-            decoration: InputDecoration(hintText: "Description "),
-            minLines: 5,
-            maxLength: 8,
+          Padding(
+            padding: const EdgeInsets.all(20),
+            child: Column(
+              children: [
+                TextField(
+                  controller: titileController,
+                  decoration: InputDecoration(hintText: "Title"),
+                ),
+                TextField(
+                  controller: descriptionController,
+                  decoration: InputDecoration(hintText: "Description "),
+                  maxLength: 8,
+                ),
+                ElevatedButton(onPressed: submitData, child: Text("Submit"))
+              ],
+            ),
           )
         ],
       ),
     );
+  }
+
+  Future<void> submitData() async {
+    //Get the data from the form
+    final title = titileController.text;
+    final description = descriptionController.text;
+
+    final body = {
+      "title": title,
+      "description": description,
+      "is_completed": false
+    };
+    //Submit data to the surver
+
+    final url = 'https://api.nstack.in/v1/todos';
+    final uri = Uri.parse(url);
+    final response = await http.post(uri,
+        body: jsonEncode(body), headers: {'Content-Type': 'application/json'});
+    print(response.statusCode);
+    print(response.body);
+
+    //Shoe ot fail message
+    if (response.statusCode == 201) {
+      titileController.text = '';
+      descriptionController.text = '';
+      showSucessMessage(" Creation sucess");
+    } else {
+      showerrorMessage(" Creation failed");
+    }
+  }
+
+  void showSucessMessage(String message) {
+    final snackBar = SnackBar(content: Text(message));
+    ScaffoldMessenger.of(context).showSnackBar(snackBar);
+  }
+
+  void showerrorMessage(String message) {
+    final snackBar = SnackBar(
+      content: Text(
+        message,
+        style: TextStyle(color: Colors.white),
+      ),
+      backgroundColor: Colors.red,
+    );
+    ScaffoldMessenger.of(context).showSnackBar(snackBar);
   }
 }
--- lib/screens/todo_list.dart
+++ lib/screens/todo_list.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:todo_api/screens/add_page.dart';
+import 'package:http/http.dart' as http;
 
 class TodoListPage extends StatefulWidget {
   const TodoListPage({super.key});
@@ -9,6 +10,13 @@ class TodoListPage extends StatefulWidget {
 }
 
 class _TodoListPageState extends State<TodoListPage> {
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    fetchTodo();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -26,5 +34,13 @@ class _TodoListPageState extends State<TodoListPage> {
   void navigateToaddPage() {
     final route = MaterialPageRoute(builder: (context) => AddTodoPage());
     Navigator.push(context, route);
+  }
+
+  Future<void> fetchTodo() async {
+    final url = 'https://api.nstack.in/v1/todos?page=1&limit=10';
+    final uri = Uri.parse(url);
+    final response = await http.get(uri);
+    print(response.statusCode);
+    print(response.body);
   }
 }
